08d9346a2d8aeb1a92986df1355ca53b282b8b0b
technerdyyy <akansha7803@gmail.com>
2024-08-21 20:39:14 +0530

    README and open functionality

---
+++ Sidebar/README.md
@@ -0,0 +1,14 @@
+## Mobile Sidebar
+
+<br><br>
+
+### Technologies used :
+<br>
+- HTML5
+<br>
+- CSS3
+<br>
+- JavaScript
+
+### Screenshot :
+<img src="demo.png">
--- Sidebar/script.js
+++ Sidebar/script.js
@@ -1,5 +1,10 @@
 const close = document.getElementById('closeSidebarBtn');
+const open = document.getElementById('openSidebarBtn');
 
 close.addEventListener('click', function() {
   document.getElementById('sidebar').style.right = '-250px';
+});
+
+open.addEventListener('click', function() {
+  document.getElementById('sidebar').style.right = 0;
 });
