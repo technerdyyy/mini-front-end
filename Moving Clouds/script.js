22d943dc5a7e2b6e60868ae662e7b9e55c7e7727
technerdyyy <akansha7803@gmail.com>
2024-08-27 19:33:02 +0530

    random size,speed to clouds

--- Moving Clouds/script.js
+++ Moving Clouds/script.js
@@ -1,23 +1,34 @@
+const containerElement = document.getElementById('container')
+containerElement.innerHTML = `
+<div class="cloud">
+    <div class="cloudBubble1"></div>
+    <div class="cloudBubble2"></div>
+</div>`.repeat(20)
+
 const elements = document.getElementsByClassName('cloud');
-const element = elements[0];
+for (let j = 0; j < elements.length; j++) {
+    const element = elements[j];
 
-element.style.top = (Math.random() * 100) + '%'
-element.style.left = (Math.random() * 100) + '%'
-element.style.transform = "scale(" + ((Math.random() * 0.7) + 0.3) + ")"
+    element.style.top = (Math.random() * 100) + '%'
+    element.style.left = (Math.random() * 100) + '%'
+    element.style.transform = "scale(" + ((Math.random() * 0.7) + 0.3) + ")"
 
-const intervalTimeInSeconds = (Math.random() * 20) + 6
-element.style.transition = "all linear " + intervalTimeInSeconds + "s"
+    const intervalTimeInSeconds = (Math.random() * 20) + 6
+    element.style.transition = "all linear " + intervalTimeInSeconds + "s"
 
-setTimeout(() => {
-    element.style.left = '100%'
-}, [100])
+    const startRight = Math.random() > 0.5
 
-let x = 1
-setInterval(() => {
-    if (x % 2 === 0) {
-        element.style.left = '100%'
-    } else {
-        element.style.left = '-50%'
-    }
-    x += 1
-}, intervalTimeInSeconds * 1000)
+    setTimeout(() => {
+        element.style.left = startRight ? '100%' : '-50%'
+    }, [100])
+
+    i = startRight ? 1 : 0
+    setInterval(() => {
+        if (i % 2 === 0) {
+            element.style.left = '100%'
+        } else {
+            element.style.left = '-50%'
+        }
+        i += 1
+    }, intervalTimeInSeconds * 1000)
+}
