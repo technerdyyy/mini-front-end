1ce7d2d1c538001d1cde7258161f7e80ad01720b
technerdyyy <akansha7803@gmail.com>
2024-08-30 23:15:47 +0530

    eval for result

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,9 +1,20 @@
+
 function appendToDisplay(value) {
   const display = document.getElementById('display');
-  display.innerText += value;
+  display.value += value;  // Appending the value to the display's innerText
 }
 
 function clearDisplay(){
     const display = document.getElementById('display');
-    display.value = " ";
+    display.value = '';  // Clearing the display's innerText by setting it to an empty string
+}
+
+function deleteLast(){
+    let display = document.getElementById('display').value;
+document.getElementById('display').value = display.substring(0, display.length - 1);  // Removing the last character from the display's innerText
+}
+
+function calculateResult(){
+    let result = eval(document.getElementById('display').value);
+    document.getElementById('display').value = result;
 }
